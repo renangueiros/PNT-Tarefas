37b8d46c243c27af289cc83d3428c9eaead42373
Renan Gueiros <renangueiros@outlook.com>
2021-11-16 23:49:05 -0300

    Adicionar interface e lógica

--- js/tarefas.js
+++ js/tarefas.js
@@ -1,6 +1,31 @@
 function Tarefas() {
+    var [tarefa, setTarefa] = React.useState('')
+    var [tarefas, setTarefas] = React.useState([])
+
+    function adicionarTarefa(e) {
+        e.preventDefault()
+        setTarefas([...tarefas, tarefa])
+    }
+
+    function pegarTarefa(e) {
+        setTarefa(e.target.value)
+    }
+
     return (
-        <h1>Tarefas</h1>
+        <div>
+            <h1>Tarefas</h1>
+            <form onSubmit={adicionarTarefa}>
+                <input onChange={pegarTarefa} value={tarefa}/>
+                <button>Adicionar</button>
+            </form>
+            <ul>
+                {tarefas.map(function (tarefa, index) {
+                    return (
+                        <li key={index}>{tarefa}</li>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
 
